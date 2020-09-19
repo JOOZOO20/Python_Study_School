77c99823d8703d06f3e7d8349c0e53d4f596dff3
JOOZOO20 <qwsa7896@naver.com>
2020-09-19 23:00:14 +0900

    으뜸 파이썬 연습문제 2.9
    -> 2주차까지 배운 내용
    1. print() 함수
    2. input() 입력함수  && 아직 반복 안배움. if조건 안배움.
    
    ​
    파이썬 전탐 과제
    으뜸 파이썬 연습문제 2.9 (책 107p)
    섭씨온도(celsius)를 화씨온도(fahrenheit)로 변환하는 식은 다음과 같다.
                                   [ farenheit = (9/5) * celsius +32 ]​
    이 식을 바탕으로 섭씨온도를 0도에서 50도까지 10도 단위로 증가시키면서 이에 해당하는 화씨온도를 다음과 같이 출력하는 프로그램 cel2fah.py를 작성하시오.
    ----------실행결과-----------
    섭씨                   화씨
    0                        32.0
    10                     50.0
    20                     68.0
    30                     86.0
    40                    104.0
    50                    122.0

---
+++ 2주차/cel2fah.py
@@ -0,0 +1,25 @@
+print('섭씨\t 화씨')
+celsius = 0
+fahrenheit = (9/5) * celsius + 32.0
+print(celsius,'\t\t', fahrenheit)
+
+celsius = 10
+fahrenheit = (9/5) * celsius + 32.0
+print(celsius,'\t\t', fahrenheit)
+
+celsius = 20
+fahrenheit = (9/5) * celsius + 32.0
+print(celsius,'\t\t', fahrenheit)
+
+celsius = 30
+fahrenheit = (9/5) * celsius + 32.0
+print(celsius,'\t\t', fahrenheit)
+
+celsius = 40
+fahrenheit = (9/5) * celsius + 32.0
+print(celsius,'\t\t', fahrenheit)
+
+celsius = 50
+fahrenheit = (9/5) * celsius + 32.0
+print(celsius,'\t\t', fahrenheit)
+
