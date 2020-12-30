664775657f94bcff8ad9fb52e9eadbdf69357d86
JOOZOO20 <qwsa7896@naver.com>
2020-12-30 23:08:29 +0900

    (책 43p) 1.3 >> 다음 파이썬 코드의 출력 결과는 무엇인가?
    (1) print(100)
    (2) print(100 + 200)
    (3) print('100 + 200')
    (4) print(100,200)
    (5) print('100','200')
    (6) print('100''200')
    (7) print('Hello Python!')

---
+++ 연습문제 1강/1.3.py
@@ -0,0 +1,42 @@
+'''#1
+100
+#2
+300
+#3
+100 + 200
+#4
+100 200
+#5
+100 200
+#6
+100200
+#7
+Hello Python!
+#8
+Hello Python !
+#9
+HelloPython!
+#10
+HelloPython!
+#11
+**********
+#12
+**********
+'''
+print(100)
+print(100+200)
+print('100 + 200')
+a=100,200
+print(a)
+b='100','200'
+print(b)
+print('100''200')
+print('Hello Python!')
+print(type(a))
+print(type(b))
+
+print('Hello','Python','!')
+print('Hello'+'Python'+'!')
+print('Hello''Python''!')
+print('**********')
+print('*'*10)
