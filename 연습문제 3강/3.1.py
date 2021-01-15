96742210ca383cb133279d28284f8adc65b1a081
JOOZOO20 <qwsa7896@naver.com>
2021-01-15 19:31:01 +0900

    (책 180p) 다음 조건문의 실행 결과는 무엇인가 ? True 혹은 False로 나타내어라.
    (1) 100 > 200
    (2) 100 >= 200
    (3) 100 < 200
    (4) 100 <= 200
    (5) 100 == 200
    (6) 100 != 200
    (7) 200 == 200
    (8) 200 != 200
    (9) True or True
    (10) True or False
    (11) True and False
    (12) True and True
    (13) True or True and False
    (14) True and True or False
    (15) 'Morning' < 'morning'
    (16) 'A' > 'B'

---
+++ 연습문제 3강/3.1.py
@@ -0,0 +1,18 @@
+print(100>200)
+print(100>=200)
+print(100<200)
+print(100<=200)
+print(100==200)
+print(100!=200)
+print(200==200)
+print(200!=200)
+print(True or True)
+print(True or False)
+print(True and False)
+print(True and True)
+print(True or True or False)
+print(True and True or False)
+print('Morning'<'morning')
+print('A'>'B')
+
+
